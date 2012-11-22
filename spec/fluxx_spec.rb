fb3130203c43e4c1b473134142dc403b1abe3957
Andy Palmer <andy@andypalmer.com>
2012-11-22 17:23:49 +0000

    andy: Refactoring, we now have a hand object

--- spec/fluxx_spec.rb
+++ spec/fluxx_spec.rb
@@ -3,9 +3,22 @@ def draw(amount, drawpile)
   drawpile.shift amount
 end
 
-def play(amount, cards)
-  cards.pop amount
-  cards
+class Hand
+  def initialize(cards)
+    @cards = cards
+  end
+
+  def play(amount, cards)
+    @cards.pop amount
+  end
+
+  def add(cards)
+    @cards += cards
+  end
+
+  def contents
+    Array.new @cards
+  end
 end
 
 def initial_hand
@@ -18,31 +31,31 @@ end
 
 describe "rules" do
   it "draws one" do
-    cards_in_hand = initial_hand
-    cards_in_hand += draw(1, initial_drawpile)
+    cards_in_hand = Hand.new(initial_hand)
+    cards_in_hand.add draw(1, initial_drawpile)
 
-    cards_in_hand.should match_array(["A", "B", "C", "X"])
+    cards_in_hand.contents.should match_array(["A", "B", "C", "X"])
   end
 
   it "plays one" do
-    cards_in_hand = initial_hand
-    cards_in_hand = play 1, cards_in_hand
+    cards_in_hand = Hand.new(initial_hand)
+    cards_in_hand.play 1, cards_in_hand
     
-    cards_in_hand.should match_array(["A", "B"])
+    cards_in_hand.contents.should match_array(["A", "B"])
   end
 
   it "draws two" do
-    cards_in_hand = initial_hand
-    cards_in_hand += draw(2, initial_drawpile)
+    cards_in_hand = Hand.new(initial_hand)
+    cards_in_hand.add draw(2, initial_drawpile)
     
-    cards_in_hand.should match_array(["A", "B", "C", "X", "Y"])
+    cards_in_hand.contents.should match_array(["A", "B", "C", "X", "Y"])
   end
 
   it "plays two" do
-    cards_in_hand = initial_hand
-    cards_in_hand = play 2, cards_in_hand
+    cards_in_hand = Hand.new(initial_hand)
+    cards_in_hand.play 2, cards_in_hand
 
-    cards_in_hand.should match_array(["A"])
+    cards_in_hand.contents.should match_array(["A"])
   end
 end
 
