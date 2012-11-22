53e1ae21b869a180b8a79dafa36f7c8551ebc80e
Andy Palmer <andy@andypalmer.com>
2012-11-22 16:44:04 +0000

    andy: Cards are now arrays instead of just a number

--- spec/fluxx_spec.rb
+++ spec/fluxx_spec.rb
@@ -1,43 +1,48 @@
 
-def draw(amount, cards)
-  cards += amount
+def draw(amount, drawpile)
+  drawpile.shift amount
 end
 
 def play(amount, cards)
-  cards -= amount
+  cards.pop amount
+  cards
 end
 
 def initial_hand
-  3
+  ["A", "B", "C"]
+end
+
+def initial_drawpile
+  ["X", "Y", "Z"]
 end
 
 describe "rules" do
   it "draws one" do
     cards_in_hand = initial_hand
-    cards_in_hand = draw 1, cards_in_hand
+    cards_in_hand += draw(1, initial_drawpile)
 
-    cards_in_hand.should eq 4
+    cards_in_hand.should match_array(["A", "B", "C", "X"])
   end
 
   it "plays one" do
     cards_in_hand = initial_hand
     cards_in_hand = play 1, cards_in_hand
     
-    cards_in_hand.should eq 2 
+    cards_in_hand.should match_array(["A", "B"])
   end
 
   it "draws two" do
     cards_in_hand = initial_hand
-    cards_in_hand = draw 2, cards_in_hand
+    cards_in_hand += draw(2, initial_drawpile)
     
-    cards_in_hand.should eq 5
+    cards_in_hand.should match_array(["A", "B", "C", "X", "Y"])
   end
 
   it "plays two" do
     cards_in_hand = initial_hand
     cards_in_hand = play 2, cards_in_hand
 
-    cards_in_hand.should eq 1
+    cards_in_hand.should match_array(["A"])
   end
 end
 
