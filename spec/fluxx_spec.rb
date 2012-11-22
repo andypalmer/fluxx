6df70da136b21c8b17a76feb37cbdb8ca577db02
Andy Palmer <andy@andypalmer.com>
2012-11-22 16:07:50 +0000

    andy: We can play two (or more) cards

--- spec/fluxx_spec.rb
+++ spec/fluxx_spec.rb
@@ -3,8 +3,8 @@ def draw(amount, cards)
   cards += amount
 end
 
-def play_one_of(cards)
-  cards -= 1
+def play(amount, cards)
+  cards -= amount
 end
 
 def initial_hand
@@ -15,13 +15,15 @@ describe "rules" do
   it "draws one" do
     cards_in_hand = initial_hand
     cards_in_hand = draw 1, cards_in_hand
+
     cards_in_hand.should eq 4
   end
 
   it "plays one" do
-    cards_in_hand = 4
-    cards_in_hand = play_one_of cards_in_hand
-    cards_in_hand.should eq 3
+    cards_in_hand = initial_hand
+    cards_in_hand = play 1, cards_in_hand
+    
+    cards_in_hand.should eq 2 
   end
 
   it "draws two" do
@@ -29,6 +31,13 @@ describe "rules" do
     cards_in_hand = draw 2, cards_in_hand
     
     cards_in_hand.should eq 5
+  end
+
+  it "plays two" do
+    cards_in_hand = initial_hand
+    cards_in_hand = play 2, cards_in_hand
+
+    cards_in_hand.should eq 1
   end
 end
 
