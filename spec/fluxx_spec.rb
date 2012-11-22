80fdd9eab197a960eca969e699e12e7b8350d93b
Andy Palmer <andy@andypalmer.com>
2012-11-22 15:55:18 +0000

    andy: First pass of refactoring

--- spec/fluxx_spec.rb
+++ spec/fluxx_spec.rb
@@ -1,14 +1,22 @@
 
+def draw_one_into(cards)
+  cards += 1
+end
+
+def play_one_of(cards)
+  cards -= 1
+end
+
 describe "rules" do
   it "draws one" do
     cards_in_hand = 3
-    cards_in_hand += 1
+    cards_in_hand = draw_one_into(cards_in_hand)
     cards_in_hand.should eq 4
   end
 
   it "plays one" do
     cards_in_hand = 4
-    cards_in_hand -= 1
+    cards_in_hand = play_one_of(cards_in_hand)
     cards_in_hand.should eq 3
   end
 end
