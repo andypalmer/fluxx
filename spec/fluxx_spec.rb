8d171f7b1d24a13730a712d19084468fc1e88911
Andy Palmer <andy@andypalmer.com>
2012-11-22 15:45:31 +0000

    andy: We can play a card (sort of)

--- spec/fluxx_spec.rb
+++ spec/fluxx_spec.rb
@@ -5,5 +5,11 @@ describe "rules" do
     cards_in_hand += 1
     cards_in_hand.should eq 4
   end
+
+  it "plays one" do
+    cards_in_hand = 4
+    cards_in_hand -= 1
+    cards_in_hand.should eq 3
+  end
 end
 
