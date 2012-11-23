66ce4594ccf3b8c47ab77771fdb4d4e4da78456a
Andy Palmer <andy@andypalmer.com>
2012-11-23 11:56:53 +0000

    andy:We can rearrange our hand

--- lib/hand.rb
+++ lib/hand.rb
@@ -14,4 +14,9 @@ class Hand
   def contents
     Array.new @cards
   end
+
+  def move(card, relative_to)
+    @cards.delete card
+    @cards.insert(relative_to.the(@cards), card)
+  end
 end
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -1,5 +1,25 @@
 require 'hand'
 
+class To
+  def To.after(card)
+    return To.new(card, 1)
+  end
+
+  def To.before(card)
+    return To.new(card, 0)
+  end
+
+  def initialize(card, relative_position)
+    @card = card
+    @relative_position = relative_position
+  end
+
+  def the(cards)
+    cards.index(@card) + @relative_position
+  end
+
+end
+
 def initial_hand
   Hand.new(["A","B","C"])
 end
@@ -24,6 +44,16 @@ describe Hand do
     hand.add ["X","Y"]
     
     hand.contents.should match_array(["A", "B", "C", "X", "Y"])
+  end
+
+  it "can be rearranged" do
+    hand = Hand.new(["A","B","C","D","E"])
+
+    hand.move("B", To.after("D"))
+    hand.contents.should eq(["A","C","D","B","E"])
+
+    hand.move("E", To.before("C"))
+    hand.contents.should eq(["A","E","C","D","B"])
   end
 end
 
